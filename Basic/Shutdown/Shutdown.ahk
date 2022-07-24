pilgrimlyieu/AutoHotkey-Script

commit ce84a0682821a5e64bd5ce8ce09d637536f602b0

`Cloze` doc
1. `Anki/Cloze`: Update document

Basic/Shutdown/Shutdown.ahk
@@ -1,18 +1,19 @@
 ﻿;@Ahk2Exe-SetMainIcon Shutdown.ico
+
 #NoTrayIcon
 
 Warn(WarnMinute) {
     Warning := "还有 " WarnMinute " 分钟就要关机！请注意保存重要内容！"
-    If ExtendLevel
+    if ExtendLevel
         Warning .= "是否要延长 " ExtendMinute " 分钟关机？"
-    Return Warning
+    return Warning
 }
 
 Hint(WarnMinute) {
     DeltaTime := SDTime
     DeltaTime -= %A_Now%, Seconds
-    Sleep % 1000 * (DeltaTime - WarnMinute * 60)
-    If WarnMinute {
+    if WarnMinute {
+        Sleep % 1000 * (DeltaTime - WarnMinute * 60)
         MsgBox % 4416 - ExtendLevel * 28, 即将关机, % Warn(WarnMinute), 10
         IfMsgBox Yes
         {
@@ -20,7 +21,7 @@ Hint(WarnMinute) {
             ExtendLevel --
         }
     }
-    Else
+    else
         Shutdown 1
 }
 
@@ -28,5 +29,5 @@ Global SDTime := A_Year A_MM A_DD 233000
 Global WarnMinutes := [5, 3, 1, 0]
 Global ExtendLevel := 1
 Global ExtendMinute := 5
-For Index, Min in WarnMinutes
+for Index, Min in WarnMinutes
     Hint(Min)

Specific/Anki/Cloze.ahk
@@ -1,15 +1,18 @@
-﻿; Cloze
-;;; Anki Cloze
-;;;;; 特性   Feature
-;;;;;;; 1. 自动清除首尾空白字符                         Auto clean left and right empty character
-;;;;;;; 2. 自动添加尾部空格                             Auto add right space
-;;;;;;; 3. 末尾字符是 } 时自动添加空格                  Auto add space if right character is }
-;;;;;;; 4. 未选中内容输入空白 Cloze 并移动光标至其中    Create empty Cloze and put cursor into it while selecting nothing
-;;;;; Anki 捷径   Anki Shortcut
-;;;;;;; F1             重置 Cloze 序数      Reset Cloze order
-;;;;;;; F2             保持 Cloze 序数      Keeping Cloze order
-;;;;;;; F3             递增 Cloze 序数      Growing Cloze order
-;;;;;;; `              保存卡片并清除序数   Save card and Reset Cloze order
+﻿; Anki Cloze
+;;; 特性   Features
+;;;;; 1. 自动清除首尾空白字符                         Automatically clean left and right empty character
+;;;;; 2. 自动添加尾部空格                             Automatically add right space
+;;;;; 3. 末尾字符是 '}'' 时自动添加空格               Automatically add space if right character is '}'
+;;;;; 4. 自动将 '}}' 改为 '} }'                       Automatically change '}}' to '} }'
+;;;;; 5. 未选中内容输入空白 Cloze 并移动光标至其中    Create empty Cloze and put cursor into it while selecting nothing
+;;; 有待支持特性 Unsupported Features
+;;;;; 1. 多光标支持                                   Multiple cursors supprot
+;;; Anki 捷径   Anki Shortcuts
+;;;;; Shift + F1            重置 Cloze 序数           Reset Cloze order
+;;;;; F5                    保持 Cloze 序数           Keeping Cloze order
+;;;;; F6                    递增 Cloze 序数           Growing Cloze order
+;;;;; `                     HTML 换行                 HTML Linebreak
+;;;;; Shift + `             保存卡片并清除序数        Save card and Reset Cloze order
 
 Cloze(keep := 0) {
     clip := Clipboard

commit ee33bc129186d4233bcb0ecac8877c5499f081d5

`Shutdown` fix
1. `Shutdown`: Fix bug of shutdown one minute ahead caused by ce84a06

Basic/Shutdown/Shutdown.ahk
@@ -12,8 +12,8 @@ Warn(WarnMinute) {
 Hint(WarnMinute) {
     DeltaTime := SDTime
     DeltaTime -= %A_Now%, Seconds
+    Sleep % 1000 * (DeltaTime - WarnMinute * 60)
     if WarnMinute {
-        Sleep % 1000 * (DeltaTime - WarnMinute * 60)
         MsgBox % 4416 - ExtendLevel * 28, 即将关机, % Warn(WarnMinute), 10
         IfMsgBox Yes
         {